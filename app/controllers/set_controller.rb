cd182adb3366f1eb6b44c28bb8534de46f8ff622
Joseph <1campbellj@gmail.com>
2019-08-04 19:40:11 -0500

    initial commit

---
+++ app/controllers/set_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class SetController < ApplicationController
+  def index; end
+end
---
+++ app/javascript/components/SetGame.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SetGame() {
+  return <div>Hey there refresh me</div>;
+}
+
+export default SetGame;
